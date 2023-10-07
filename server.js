2be8b0c8c7f372f39d89c38c4da87973f717666b
Mario <mariokozul24@gmail.com>
2023-10-07 20:39:00 +0200

    added sign-up, log-in and session implementation

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { connectDB,connection } from './config/database.js';
 import cors from 'cors';
+import { checkEmail, checkUsername, checkPassword } from './middleware/serverMiddleware.js';
+import { User } from './models/users.js';
+import { generatePassword } from './lib/passwordUtils.js';
+import { generateKeys } from './lib/keys.js';
+import MongoStore from 'connect-mongo';
+import passport from 'passport';
+import session from 'express-session';
 
 config();
 
@@ -22,6 +29,31 @@ app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+//session setup
+const mongoStore = MongoStore.create({
+    mongoUrl: process.env.DB_STRING,
+    collectionName: 'sessions',
+    mongooseConnection: connection
+})
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: mongoStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
+
+//END session setup
+
+//passport auth
+import './config/passport.js';
+app.use(passport.initialize());
+app.use(passport.session())
+//END passport auth
+
 app.get('/', async (req,res) => {
     const filePath = __dirname + '/public/sign-up.html';
     res.sendFile(filePath);
@@ -32,9 +64,28 @@ app.get('/log-in', (req,res) => {
     res.sendFile(filePath);
 })
 
-app.post('/sign-up', (req,res) => {
-    console.log(req.body);
-    res.json({bla: true});
+app.post('/sign-up', checkUsername, checkEmail, checkPassword, async (req,res) => {
+    const password = generatePassword(req.body.password1);
+    const keys = generateKeys();
+    try{
+        const newUser = new User({
+            email: req.body.email,
+            username: req.body.username,
+            hash: password.hash,
+            salt: password.salt,
+            privateKey: keys.privateKey,
+            publicKey: keys.publicKey
+        })
+        const created = await newUser.save();
+        res.json({err: false, msg:'User successfully created'});
+        
+    } catch(err){
+        console.log(err);
+    }
+})
+
+app.post('/log-in', passport.authenticate('local'), (req,res) => {
+    res.json({err: false, msg:'Successfully logged in'});
 })
 
 
