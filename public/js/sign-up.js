8f5fda05b05f3256bc84442ab79309fdbcba702a
Mario <mariokozul24@gmail.com>
2023-10-07 20:44:36 +0200

    DOM manipulation on client side

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -1,29 +1,45 @@
 const form = document.querySelector('#sign-up-form');
-form.addEventListener('submit', sendData);
+const p = document.querySelector('#error-success-para');
+
+const changeDOM = (res) => {
+  if(res.err){
+    p.style.setProperty('color', 'red');
+    p.innerHTML = res.msg;
+  }
+  else if(typeof res.err === 'undefined'){
+    p.style.setProperty('color', 'black');
+    p.innerHTML = '';
+  }
+  else {
+    p.style.setProperty('color', 'green');
+    p.innerHTML = res.msg;
+  }
+}
 
 const sendData = async (e) => {
-    e.preventDefault();
-    const data = new FormData(form);
-    console.log(data);
-    const jsonData = {};
-    data.forEach((value, key) => {
-      jsonData[key] = value;
-    });
-    try{
-        const res = await fetch(
-            '/sign-up',
-            {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(jsonData)
+  e.preventDefault();
+  const data = new FormData(form);
+  const jsonData = {};
+  data.forEach((value, key) => {
+    jsonData[key] = value;
+  });
+  try{
+      const res = await fetch(
+          '/sign-up',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
             },
-          );
-        const resData = await res.json();
-        console.log(resData);
-    } catch(err){
-        console.log(err.message);
-    }
-    
+            body: JSON.stringify(jsonData)
+          },
+        );
+      const resData = await res.json();
+      if(resData) changeDOM(resData);
+      if(resData) form.reset();
+  } catch(err){
+      console.log(err);
+  }
 }
+
+form.addEventListener('submit', sendData);
