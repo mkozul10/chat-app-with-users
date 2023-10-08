8e2ce31399c853559a1c38f122ee3ccd82aa079c
Mario <mariokozul24@gmail.com>
2023-10-08 21:48:31 +0200

    fetch api for loging-in

---
+++ public/js/log-in.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('#log-in-form');
+const p = document.querySelector('#error-success-para');
+
+const changeDOM = (res) => {
+  if(res.err){
+    p.style.setProperty('color', 'red');
+    p.innerHTML = res.msg;
+  }
+  else if(typeof res.err === 'undefined'){
+    p.style.setProperty('color', 'black');
+    p.innerHTML = '';
+  }
+  else {
+    p.style.setProperty('color', 'green');
+    p.innerHTML = res.msg;
+  }
+}
+
+const sendData = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  const jsonData = {};
+  data.forEach((value, key) => {
+    jsonData[key] = value;
+  });
+  try{
+      const res = await fetch(
+          '/log-in',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(jsonData)
+          },
+        );
+      const resData = await res.json();
+      if(resData) changeDOM(resData);
+      if(resData) form.reset();
+  } catch(err){
+      console.log(err);
+  }
+}
+
+form.addEventListener('submit', sendData);
